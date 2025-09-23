e42d7a14078ed6138ce8efe0ebc029b0dab965c6
hannahsamuel07 <hannahsamuel@ucla.edu>
2025-09-23 16:35:45 -0700

    loops, funcs, and var scope

--- todolistapp.go
+++ todolistapp.go
@@ -6,12 +6,29 @@ func main() {
 	var shortGolang = "Watch Go crash course"
 	var fullGolang = "Watch Nana's Goland Full Course"
 	var rewardDessert = "Reward myself with a donut"
+	var maxItems = 20
 	// array is fixed size put a numberin the brackets, if u put more than the number it goes out of index
 	//slice is dynamic array
-	taskItem := []string{shortGolang, fullGolang, rewardDessert}
+	var taskItem = []string{shortGolang, fullGolang, rewardDessert}
 	fmt.Println(("##### Welcome to our Todolist App! #####"))
-
+	printTask(taskItem, maxItems)
+	fmt.Println()
+	addTask(taskItem, "Go for a run")
+	fmt.Println()
+	printTask(taskItem, 20)
+}
+func printTask(taskItem []string, itemLimit int) {
 	fmt.Println("List of my Todos")
-	fmt.Println("Tasks:", taskItem)
+	//for loop in go, index and range keyword
+	// index, -> _, //when we dont need to use the index
+	for index, task := range taskItem {
+		//fmt.Println(index+1, ".", task)
+		fmt.Printf("%d: %s\n", index+1, task) // print f for string modication for formatting
 
+	}
+
+}
+func addTask(taskItem []string, newTask string) {
+	var updatedTaskItems = append(taskItem, newTask)
+	printTask(updatedTaskItems, 20)
 }
