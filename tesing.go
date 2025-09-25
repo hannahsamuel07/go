317e719b5a3b89396712f7f469d7f5d09aa44a0e
hannahsamuel07 <hannahsamuel@ucla.edu>
2025-09-24 19:38:57 -0700

    build HTTP endpoints and start web server to serve requests

--- tesing.go
+++
@@ -1,7 +0,0 @@
-package main //indicates file belongs to an executable program
-
-import "fmt"
-
-func test() {
-	fmt.Print("Hello from Go in VS Code! This is Hannah starting Golang :)")
-}
--- todolistapp.go
+++ todolistapp.go
@@ -1,26 +1,51 @@
 package main //indicates file belongs to an executable program
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+var shortGolang = "Watch Go crash course"
+var fullGolang = "Watch Nana's Goland Full Course"
+var rewardDessert = "Reward myself with a donut"
+var taskItem = []string{shortGolang, fullGolang, rewardDessert}
 
 func main() {
-	var shortGolang = "Watch Go crash course"
-	var fullGolang = "Watch Nana's Goland Full Course"
-	var rewardDessert = "Reward myself with a donut"
-	var maxItems = 20
+
+	//var maxItems = 20
 	// array is fixed size put a numberin the brackets, if u put more than the number it goes out of index
 	//slice is dynamic array
-	var taskItem = []string{shortGolang, fullGolang, rewardDessert}
 	fmt.Println(("##### Welcome to our Todolist App! #####"))
-	printTask(taskItem, maxItems)
-	fmt.Println()
-	//update list with new tasks
-	taskItem = addTask(taskItem, "Go for a run")
-	taskItem = addTask(taskItem, "Practice coding in Go")
+	http.HandleFunc("/", home)
 
-	fmt.Println()
-	printTask(taskItem, 20)
+	http.HandleFunc("/show-tasks", showTasks)
+
+	http.HandleFunc("/hello-go", helloUser)
+	http.ListenAndServe(":8080", nil)
+	//printTask(taskItem)
+	//fmt.Println()
+	//update list with new tasks
+	//taskItem = addTask(taskItem, "Go for a run")
+	//taskItem = addTask(taskItem, "Practice coding in Go")
+
+	//fmt.Println()
+	//printTask(taskItem)
 }
-func printTask(taskItem []string, itemLimit int) {
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to the Todo App!")
+	fmt.Fprintln(w, "Try /hello-go or /show-tasks")
+}
+func showTasks(writer http.ResponseWriter, request *http.Request) {
+	for _, task := range taskItem {
+		fmt.Fprintln(writer, task)
+	}
+}
+func helloUser(writer http.ResponseWriter, request *http.Request) {
+	var greeting = "Hello user. Welcome to our TodolistApp!"
+	fmt.Fprintln(writer, greeting)
+}
+
+/*func printTask(taskItem []string) {
 	fmt.Println("List of my Todos")
 	//for loop in go, index and range keyword
 	// index, -> _, //when we dont need to use the index
@@ -35,6 +60,6 @@ func printTask(taskItem []string, itemLimit int) {
 // inputs expected					//type of output expected
 func addTask(taskItem []string, newTask string) []string {
 	var updatedTaskItems = append(taskItem, newTask)
-	//printTask(updatedTaskItems, 20)
 	return updatedTaskItems
 }
+*/
